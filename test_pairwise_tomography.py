a1ff6cc8b63b100ceceb39873f52e4358040ac29
Matteo Rossi <teo.red90@gmail.com>
2019-11-22 11:10:43 +0200

    Switched to unittest

--- test_pairwise_tomography.py
+++ test_pairwise_tomography.py
@@ -1,44 +1,124 @@
-import pytest 
+
+# pylint: disable=missing-docstring
+import unittest
+
+import numpy as np
+import qiskit
+from qiskit import QuantumRegister, QuantumCircuit, execute, Aer
+from qiskit.quantum_info import state_fidelity
+from qiskit.tools.qi.qi import partial_trace
+from qiskit.quantum_info.states import DensityMatrix
 
 from pairwise_tomography.pairwise_state_tomography_circuits import pairwise_state_tomography_circuits
 from pairwise_tomography.pairwise_fitter import PairwiseStateTomographyFitter
 
-from qiskit import QuantumCircuit, QuantumRegister
-from qiskit import execute
-from qiskit import Aer
+n_list = [2, 4]
+nshots = 5000
 
-from qiskit.quantum_info import state_fidelity
-from qiskit.tools.qi.qi import partial_trace
-import numpy as np
+    
+class TestPairwiseStateTomography(unittest.TestCase):
+    def test_pairwise_tomography(self):
+        for n in n_list:
+            with self.subTest():
+                self.tomography_random_circuit(n)
+    
+    def tomography_random_circuit(self, n):
+        q = QuantumRegister(n)
+        qc = QuantumCircuit(q)
 
-from qiskit.quantum_info.states import DensityMatrix
+        psi = ((2 * np.random.rand(2 ** n) - 1) 
+            + 1j * (2 *np.random.rand(2 ** n) - 1))
+        psi /= np.linalg.norm(psi)
 
-np.set_printoptions(suppress=True)
+        qc.initialize(psi, q)
+        rho = DensityMatrix.from_instruction(qc).data
 
-@pytest.mark.parametrize("n", range(3,7))
-@pytest.mark.parametrize("nshots", [1000, 5000])
-def test_pairwise_fitter(n, nshots):
-    q = QuantumRegister(n)
-    qc = QuantumCircuit(q)
+        circ = pairwise_state_tomography_circuits(qc, q)
+        job = execute(circ, Aer.get_backend("qasm_simulator"), shots=nshots)
+        fitter = PairwiseStateTomographyFitter(job.result(), circ, q)
+        result = fitter.fit()
 
-    psi = ((2 * np.random.rand(2 ** n) - 1) 
-           + 1j * (2 *np.random.rand(2 ** n) - 1))
-    psi /= np.linalg.norm(psi)
-    qc.initialize(psi, q)
-    rho = DensityMatrix.from_instruction(qc).data
+        # Compare the tomography matrices with the partial trace of 
+        # the original state using fidelity
+        for (k, v) in result.items():
+            trace_qubits = list(range(n))
+            trace_qubits.remove(k[0])
+            trace_qubits.remove(k[1])
+            rhok = partial_trace(rho, trace_qubits)
+            f = state_fidelity(v, rhok)
+            try:
+                self.assertAlmostEqual(f, 1, delta=1 / np.sqrt(nshots))
+            except AssertionError:
+                print(k, f)
+                raise
+            
+    def test_meas_qubit_specification(self):
+        n = 4
 
-    circ = pairwise_state_tomography_circuits(qc, q)
-    job = execute(circ, Aer.get_backend("qasm_simulator"), shots=nshots)
-    fitter = PairwiseStateTomographyFitter(job.result(), circ, q)
-    result = fitter.fit()
-    print("Results fit")
+        q = QuantumRegister(n)
+        qc = QuantumCircuit(q)
 
-    # Compare the tomography matrices with the partial trace of the original 
-    # state.
-    for (k, v) in result.items():
-        trace_qubits = list(range(n))
-        trace_qubits.remove(k[0])
-        trace_qubits.remove(k[1])
-        rhok = partial_trace(rho, trace_qubits)
-        print(k, 1 - state_fidelity(v, rhok))
-        assert 1 - state_fidelity(v, rhok) < 1 / np.sqrt(nshots)
+        psi = ((2 * np.random.rand(2 ** n) - 1) 
+            + 1j * (2 *np.random.rand(2 ** n) - 1))
+        psi /= np.linalg.norm(psi)
+
+        qc.initialize(psi, q)
+        rho = DensityMatrix.from_instruction(qc).data
+
+        measured_qubits = q#[q[0], q[1], q[2]]
+        circ = pairwise_state_tomography_circuits(qc, measured_qubits)
+        job = execute(circ, Aer.get_backend("qasm_simulator"), shots=nshots)
+        fitter = PairwiseStateTomographyFitter(job.result(), circ, measured_qubits)
+        result = fitter.fit()
+
+        # Compare the tomography matrices with the partial trace of 
+        # the original state using fidelity
+        for (k, v) in result.items():
+            trace_qubits = list(range(n))
+            trace_qubits.remove(measured_qubits[k[0]].index)
+            trace_qubits.remove(measured_qubits[k[1]].index)
+            rhok = partial_trace(rho, trace_qubits)
+            f = state_fidelity(v, rhok)
+            try:
+                self.assertAlmostEqual(f, 1, delta=1 / np.sqrt(nshots))
+            except AssertionError:
+                print(k, f)
+                raise
+
+    def test_multiple_registers(self):
+        n = 4
+
+        q = QuantumRegister(2)
+        p = QuantumRegister(2)
+
+        qc = QuantumCircuit(q, p)
+        
+        qc.h(q[0])
+        qc.rx(np.pi/4, q[1])
+        qc.cx(q[0], p[0])
+        qc.cx(q[1], p[1])
+
+        rho = DensityMatrix.from_instruction(qc).data
+
+        measured_qubits = q#[q[0], q[1], q[2]]
+        circ = pairwise_state_tomography_circuits(qc, measured_qubits)
+        job = execute(circ, Aer.get_backend("qasm_simulator"), shots=nshots)
+        fitter = PairwiseStateTomographyFitter(job.result(), circ, measured_qubits)
+        result = fitter.fit()
+
+        # Compare the tomography matrices with the partial trace of 
+        # the original state using fidelity
+        for (k, v) in result.items():
+            trace_qubits = list(range(n))
+            trace_qubits.remove(measured_qubits[k[0]].index)
+            trace_qubits.remove(measured_qubits[k[1]].index)
+            rhok = partial_trace(rho, trace_qubits)
+            f = state_fidelity(v, rhok)
+            try:
+                self.assertAlmostEqual(f, 1, delta=1 / np.sqrt(nshots))
+            except AssertionError:
+                print(k, f)
+                raise
+
+if __name__ == '__main__':
+    unittest.main()
