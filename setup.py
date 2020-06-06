b67835f2db81ef30c9cceb1eff9d910a241c2b31
Matteo Rossi <teo.red90@gmail.com>
2020-06-06 22:50:50 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='pairwise-tomography',
       install_requires=['qiskit>=0.12', 'scipy', 'matplotlib', 'networkx'],
-      version='0.0.1',
+      version='0.0.2',
       packages=[package for package in find_packages()
                 if package.startswith('pairwise_tomography')]
 )
