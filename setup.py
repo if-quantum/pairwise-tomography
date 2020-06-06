46427e3c1d25c09ed34f389b4f3288cd500380b2
Matteo Rossi <teo.red90@gmail.com>
2020-06-06 22:52:00 +0300

    Update qiskit to 0.19

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pairwise-tomography',
-      install_requires=['qiskit>=0.12', 'scipy', 'matplotlib', 'networkx'],
+      install_requires=['qiskit>=0.19', 'scipy', 'matplotlib', 'networkx'],
       version='0.0.2',
       packages=[package for package in find_packages()
                 if package.startswith('pairwise_tomography')]
